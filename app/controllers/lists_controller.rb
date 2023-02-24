fdd4a0e7ae3ffa5c6a62f6bd6ec3cfc20bae2824
Salvador Cabral <salvadormhoc@gmail.com>
2023-02-24 10:57:05 +0000

    rails watch end 1st day

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,29 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: [:show]
+
+  def index
+    @lists = List.all
+  end
+
+  def show; end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to lists_path(@list)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create destroy]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
